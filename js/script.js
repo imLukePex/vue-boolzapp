a54a5d80dd50b83f01f02ea27279e9d98697932d
imLukePex <luca.pessione@gmail.com>
2023-12-17 20:49:59 +0100

    added click to the contact

--- js/script.js
+++ js/script.js
@@ -39,17 +39,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '10/01/2020 16:15',
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -61,17 +61,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '20/03/2020 16:30',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '20/03/2020 16:30',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '20/03/2020 16:35',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -83,17 +83,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '28/03/2020 10:10',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '28/03/2020 10:20',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '28/03/2020 16:15',
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -105,12 +105,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -122,12 +122,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -139,17 +139,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Ciao Harlow, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020 15:51',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -161,12 +161,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -178,17 +178,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020 15:51',
                             message: 'OK!!',
                             status: 'received'
                         }
@@ -200,17 +200,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Ciao, quando fai uscire il prossimo album?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Spero presto...!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020 15:51',
                             message: 'Perfetto!!!',
                             status: 'received'
                         }
@@ -221,12 +221,8 @@ createApp({
         }
     },
     methods: {
-        colorMsg(i) {
-            if(this.contacts[activeContact].messages[i].status === 'sent') {
-                this.contacts[activeContact].messages[i].status = 'received';
-            } else {
-                this.contacts[activeContact].messages[i].status = 'sent';
-            }
-        }
+        activeChat(i) {
+            this.activeContact = i;
+        },
     }
 }).mount("#app")
